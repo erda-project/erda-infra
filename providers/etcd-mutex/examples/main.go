cdb207c498820b931f558863285ae26284ad1552
RecallSong <13607438+recallsong@users.noreply.github.com>
2021-09-14 14:22:36 +0800

    fix etcd mutex (#93)

--- providers/etcd-mutex/examples/main.go
+++ providers/etcd-mutex/examples/main.go
@@ -30,24 +30,61 @@ type provider struct {
 	Lock  mutex.Mutex     `mutex-key:"test-key"` // autowired
 }
 
-func (p *provider) Init(ctx servicehub.Context) error {
-	fmt.Println(p.Mutex)
+func (p *provider) Run(ctx context.Context) error {
+	go func() {
+		time.Sleep(10 * time.Second)
+		err := p.Lock.Close()
+		if err != nil {
+			fmt.Println("Close err: ", err)
+			return
+		}
+		err = p.Lock.Close()
+		if err != nil {
+			fmt.Println("Close err: ", err)
+			return
+		}
+	}()
+	lock := p.Lock
+	sleep := func(d time.Duration) bool {
+		select {
+		case <-time.After(d):
+		case <-ctx.Done():
+			return false
+		}
+		return true
+	}
+	doTaskInLock := func(prefix string) bool {
+		err := lock.Lock(ctx)
+		if err != nil {
+			fmt.Println("Lock err: ", err)
+			return false
+		}
+		defer func() {
+			err := lock.Unlock(context.TODO())
+			if err != nil {
+				fmt.Println("Unlock err: ", err)
+			}
+		}()
+		fmt.Println(prefix+" {", time.Now())
+		if !sleep(1 * time.Second) {
+			return false
+		}
+		fmt.Println(prefix+"    ", time.Now())
+		if !sleep(1 * time.Second) {
+			return false
+		}
+		fmt.Println(prefix+" }", time.Now())
+		return true
+	}
 	var wg sync.WaitGroup
-	wg.Add(2)
-	go func() {
-		defer wg.Done()
-		defer p.Lock.Unlock(context.TODO())
-		p.Lock.Lock(context.TODO())
-		fmt.Println("A", time.Now())
-		time.Sleep(5 * time.Second)
-	}()
-	go func() {
-		defer wg.Done()
-		defer p.Lock.Unlock(context.TODO())
-		p.Lock.Lock(context.TODO())
-		fmt.Println("B", time.Now())
-		time.Sleep(5 * time.Second)
-	}()
+	for _, elem := range []string{"A", "B", "C"} {
+		wg.Add(1)
+		go func(prefix string) {
+			defer wg.Done()
+			for doTaskInLock(prefix) {
+			}
+		}(elem)
+	}
 	wg.Wait()
 	return nil
 }
@@ -69,10 +106,26 @@ func main() {
 }
 
 // OUTPUT:
-// INFO[2021-03-18 17:18:46.578] provider etcd initialized
-// INFO[2021-03-18 17:18:46.578] provider etcd-mutex (depends [etcd]) initialized
-// &{0xc00000ffe0 0xc0002f9800 0xc0002b8630 map[] 0xc0002f6070}
-// B 2021-03-18 17:18:46.772563 +0800 CST m=+0.202808470
-// A 2021-03-18 17:18:51.804971 +0800 CST m=+5.235190119
-// INFO[2021-03-18 17:18:56.852] provider example (depends [etcd-mutex]) initialized
-// INFO[2021-03-18 17:18:56.852] signals to quit:[hangup interrupt terminated quit]
+// INFO[2021-09-13 17:13:51.818] provider etcd initialized
+// INFO[2021-09-13 17:13:51.818] provider etcd-mutex (depends services: [etcd]) initialized
+// INFO[2021-09-13 17:13:51.818] provider example (depends services: [etcd-mutex], providers: [etcd-mutex etcd-mutex]) initialized
+// INFO[2021-09-13 17:13:51.818] signals to quit: [hangup interrupt terminated quit]
+// INFO[2021-09-13 17:13:51.820] provider example running ...
+// A { 2021-09-13 17:13:51.846363 +0800 CST m=+0.082408900
+// A     2021-09-13 17:13:52.847656 +0800 CST m=+1.083695286
+// A } 2021-09-13 17:13:53.848607 +0800 CST m=+2.084640009
+// C { 2021-09-13 17:13:53.872487 +0800 CST m=+2.108520071
+// C     2021-09-13 17:13:54.87288 +0800 CST m=+3.108907011
+// C } 2021-09-13 17:13:55.874864 +0800 CST m=+4.110884363
+// B { 2021-09-13 17:13:55.898284 +0800 CST m=+4.134303984
+// B     2021-09-13 17:13:56.902232 +0800 CST m=+5.138246123
+// B } 2021-09-13 17:13:57.904445 +0800 CST m=+6.140452108
+// A { 2021-09-13 17:13:57.925672 +0800 CST m=+6.161679201
+// A     2021-09-13 17:13:58.930195 +0800 CST m=+7.166196424
+// A } 2021-09-13 17:13:59.933883 +0800 CST m=+8.169877844
+// C { 2021-09-13 17:13:59.956048 +0800 CST m=+8.192042605
+// C     2021-09-13 17:14:00.960022 +0800 CST m=+9.196010725
+// Lock err:  mutex closed
+// Lock err:  mutex closed
+// Lock err:  mutex closed
+// INFO[2021-09-13 17:14:01.963] provider example Run exit
