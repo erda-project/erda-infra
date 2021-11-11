57e90c3e2a94874f21fc6aa4fe8af8c40c717363
sfwn <sfwnlin@gmail.com>
2021-11-11 17:30:36 +0800

    cp: refactor yet-another-demo example

---
+++ providers/component-protocol/examples/components/yet_another_demo/demotable/provider.go
@@ -0,0 +1,68 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package demotable
+
+import (
+	"reflect"
+
+	"github.com/erda-project/erda-infra/base/servicehub"
+	"github.com/erda-project/erda-infra/providers/component-protocol/protocol"
+)
+
+// Interface export ability for demotable
+type Interface interface {
+	protocol.CompRender
+}
+
+type config struct {
+	Scenario string
+	Name     string
+}
+
+type provider struct {
+	Cfg *config
+}
+
+// Init .
+func (p *provider) Init(ctx servicehub.Context) error {
+	compName := "demoTable"
+	if ctx.Label() != "" {
+		compName = ctx.Label()
+	}
+	protocol.MustRegisterComponent(&protocol.CompRenderSpec{
+		Scenario: p.Cfg.Scenario,
+		CompName: compName,
+		RenderC:  func() protocol.CompRender { return &provider{} },
+	})
+	return nil
+}
+
+// Provide .
+func (p *provider) Provide(ctx servicehub.DependencyContext, args ...interface{}) interface{} {
+	return p
+}
+
+func init() {
+	interfaceType := reflect.TypeOf((*Interface)(nil)).Elem()
+	servicehub.Register("component-protocol.components.yet-another-demo.table", &servicehub.Spec{
+		Types: []reflect.Type{interfaceType},
+		ConfigFunc: func() interface{} {
+			return &config{}
+		},
+		Creator: func() servicehub.Provider {
+			return &provider{}
+		},
+	})
+}
