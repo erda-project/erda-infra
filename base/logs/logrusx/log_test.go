bff740bc1550cd7fdb06b39223f637dc1e637977
innerpeacez <innerpeace.zhai@gmail.com>
2021-12-03 10:40:37 +0800

    formart

---
+++ base/logs/logrusx/log_test.go
@@ -0,0 +1,25 @@
+package logrusx
+
+import (
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	type args struct {
+		options []Option
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"case", args{options: []Option{&option{logrus.InfoLevel}}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := New(tt.args.options...)
+			got.Info("This will go to stdout")
+			got.Warn("This will go to stderr")
+		})
+	}
+}
