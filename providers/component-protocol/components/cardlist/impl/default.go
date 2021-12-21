5fb638f4a9696f12bdd775c150ed0a2d1f0448a7
bugaolengdeyuxiaoer <46627662+bugaolengdeyuxiaoer@users.noreply.github.com>
2021-12-21 18:20:23 +0800

    feat(component-protocol): add component cardlist (#151)
    * feat(component-protocol): add component cardlist
    
    * refactor(component protocol): change struct and field name, add some annotation
    
    * refactor(component protocol): add json tag
    
    Co-authored-by: erda-bot <81558540+erda-bot@users.noreply.github.com>

---
+++ providers/component-protocol/components/cardlist/impl/default.go
@@ -0,0 +1,90 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package impl
+
+import (
+	"github.com/erda-project/erda-infra/providers/component-protocol/components/cardlist"
+	"github.com/erda-project/erda-infra/providers/component-protocol/cptype"
+	"github.com/erda-project/erda-infra/providers/component-protocol/utils/cputil"
+)
+
+// DefaultCard .
+type DefaultCard struct {
+	Impl cardlist.ICard
+
+	*StdStructuredPtr
+}
+
+// StdStructuredPtr .
+type StdStructuredPtr struct {
+	StdInParamsPtr *cptype.ExtraMap
+	StdDataPtr     *cardlist.Data
+	StdStatePtr    *cptype.ExtraMap
+}
+
+// DataPtr .
+func (s *StdStructuredPtr) DataPtr() interface{} { return s.StdDataPtr }
+
+// StatePtr .
+func (s *StdStructuredPtr) StatePtr() interface{} { return s.StdStatePtr }
+
+// InParamsPtr .
+func (s *StdStructuredPtr) InParamsPtr() interface{} { return s.StdInParamsPtr }
+
+// RegisterCompStdOps .
+func (d *DefaultCard) RegisterCompStdOps() (opFuncs map[cptype.OperationKey]cptype.OperationFunc) {
+	return map[cptype.OperationKey]cptype.OperationFunc{
+		cardlist.OpCardListGoto{}.OpKey(): func(sdk *cptype.SDK) {
+			d.Impl.RegisterCardGotoOp(*cputil.MustObjJSONTransfer(sdk.Event.OperationData, &cardlist.OpCardListGoto{}).(*cardlist.OpCardListGoto))(sdk)
+		},
+		cardlist.OpCardListIconGoto{}.OpKey(): func(sdk *cptype.SDK) {
+			d.Impl.RegisterCardIconGotoOp(*cputil.MustObjJSONTransfer(sdk.Event.OperationData, &cardlist.OpCardListIconGoto{}).(*cardlist.OpCardListIconGoto))(sdk)
+		},
+	}
+}
+
+// RegisterCompNonStdOps .
+func (d *DefaultCard) RegisterCompNonStdOps() (opFuncs map[cptype.OperationKey]cptype.OperationFunc) {
+	return nil
+}
+
+// Initialize .
+func (d *DefaultCard) Initialize(sdk *cptype.SDK) {}
+
+// Finalize .
+func (d *DefaultCard) Finalize(sdk *cptype.SDK) {}
+
+// SkipOp providers default impl for user.
+func (d *DefaultCard) SkipOp(sdk *cptype.SDK) bool { return !d.Impl.Visible(sdk) }
+
+// BeforeHandleOp providers default impl for user.
+func (d *DefaultCard) BeforeHandleOp(sdk *cptype.SDK) {}
+
+// AfterHandleOp providers default impl for user.
+func (d *DefaultCard) AfterHandleOp(sdk *cptype.SDK) {}
+
+// StdStructuredPtrCreator .
+func (d *DefaultCard) StdStructuredPtrCreator() func() cptype.IStdStructuredPtr {
+	return func() cptype.IStdStructuredPtr {
+		return &StdStructuredPtr{
+			StdInParamsPtr: &cptype.ExtraMap{},
+			StdStatePtr:    &cptype.ExtraMap{},
+			StdDataPtr:     &cardlist.Data{},
+		}
+	}
+}
+
+// Visible .
+func (d *DefaultCard) Visible(sdk *cptype.SDK) bool { return true }
---
+++ providers/component-protocol/components/cardlist/operation.go
@@ -0,0 +1,52 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cardlist
+
+import (
+	"github.com/erda-project/erda-infra/providers/component-protocol/cptype"
+)
+
+type (
+	// OpCardListGoto define click operation of cardList .
+	OpCardListGoto struct {
+		cptype.Operation
+		ServerData OpCardListGotoData `json:"serverData,omitempty"`
+	}
+
+	// OpCardListGotoData define click operation data of cardList .
+	OpCardListGotoData struct {
+		// if JumpOut is true ,open new window, else open slide window
+		JumpOut bool            `json:"jumpOut"`
+		Target  string          `json:"target"`
+		Query   cptype.ExtraMap `json:"query,omitempty"`
+		Params  cptype.ExtraMap `json:"params,omitempty"`
+	}
+)
+
+type (
+	// OpCardListIconGoto define click operation of icon in cardList.
+	OpCardListIconGoto struct {
+		cptype.Operation
+		Icon       string             `json:"icon"`
+		Tip        string             `json:"tip"`
+		ServerData OpCardListGotoData `json:"serverData,omitempty"`
+	}
+)
+
+// OpKey .
+func (o OpCardListGoto) OpKey() cptype.OperationKey { return "clickGoto" }
+
+// OpKey .
+func (o OpCardListIconGoto) OpKey() cptype.OperationKey { return "clickGoto" }
