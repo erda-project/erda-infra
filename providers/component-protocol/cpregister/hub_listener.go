96b0c7090f0b35fa2abfaad9ff75f6b42310f85b
sfwn <sfwnlin@gmail.com>
2022-01-05 23:00:55 +0800

    cp: add hubListener to auto register comp to hub config

---
+++ providers/component-protocol/cpregister/hub_listener.go
@@ -0,0 +1,48 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cpregister
+
+import (
+	"embed"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/erda-project/erda-infra/base/servicehub"
+)
+
+// AllExplicitProviderCreatorMap contains all user specified provider.
+var AllExplicitProviderCreatorMap = map[string]servicehub.Provider{}
+
+type HubListener struct {
+	ScenarioFSs []embed.FS
+}
+
+func NewHubListener(fs ...embed.FS) *HubListener {
+	return &HubListener{ScenarioFSs: fs}
+}
+
+func (l *HubListener) BeforeInitialization(h *servicehub.Hub, config map[string]interface{}) error {
+	logrus.Info("auto register component provider to hub.config")
+	// auto register explicit component provider firstly
+	for providerName, creator := range AllExplicitProviderCreatorMap {
+		config[providerName] = creator
+		logrus.Infof("auto register component provider to hub.config: %s", providerName)
+	}
+	return nil
+}
+
+func (l *HubListener) AfterInitialization(h *servicehub.Hub) error   { return nil }
+func (l *HubListener) AfterStart(h *servicehub.Hub) error            { return nil }
+func (l *HubListener) BeforeExit(h *servicehub.Hub, err error) error { return nil }
