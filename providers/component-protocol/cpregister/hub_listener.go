c47da5c80fc7a8da8fb3eaa9f4fd53581055b17b
sfwn <sfwnlin@gmail.com>
2022-01-06 11:20:02 +0800

    cp: set logrus time format for HubListener

--- providers/component-protocol/cpregister/hub_listener.go
+++ providers/component-protocol/cpregister/hub_listener.go
@@ -61,3 +61,7 @@ func (l *HubListener) AfterStart(h *servicehub.Hub) error { return nil }
 
 // BeforeExit .
 func (l *HubListener) BeforeExit(h *servicehub.Hub, err error) error { return nil }
+
+func init() {
+	logrus.SetFormatter(&logrus.TextFormatter{FullTimestamp: true, TimestampFormat: "2006-01-02 15:04:05.000"})
+}
