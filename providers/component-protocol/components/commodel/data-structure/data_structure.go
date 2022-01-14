4ec5d0640d29ef05bdb95a313d9266ae31095d4e
innerpeacez <innerpeace.zhai@gmail.com>
2022-01-14 17:37:55 +0800

    feat: linegraph and bubblegraph auto unit (#203)
    * linegraph and bubblegraph auto unit
    
    * optimize
    
    fix demo
    
    go imports format
    
    go imports format
    
    go imports format
    
    * mv to commodel
    
    * go imports format
    
    * fix ci
    
    * fix ci

---
+++ providers/component-protocol/components/commodel/data-structure/data_structure.go
@@ -0,0 +1,71 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package structure
+
+//Type defined
+const (
+	String      Type = "string"
+	Number      Type = "number"
+	Capacity    Type = "capacity"
+	TrafficRate Type = "trafficRate"
+	Storage     Type = "storage"
+	Timestamp   Type = "timestamp"
+	Rate        Type = "rate"
+	Date        Type = "date"
+	Percent     Type = "percent"
+	Throughput  Type = "throughput"
+)
+
+//Precision defined
+const (
+	K           Precision = "K"
+	M           Precision = "M"
+	B           Precision = "B"
+	KB          Precision = "KB"
+	MB          Precision = "MB"
+	GB          Precision = "GB"
+	TB          Precision = "TB"
+	PB          Precision = "PB"
+	EB          Precision = "EB"
+	ZB          Precision = "ZB"
+	YB          Precision = "YB"
+	BSlashS     Precision = "B/s"
+	KBSlashS    Precision = "KB/s"
+	MBSlashS    Precision = "MB/s"
+	GBSlashS    Precision = "GB/s"
+	TBSlashS    Precision = "TB/s"
+	PBSlashS    Precision = "PB/s"
+	EBSlashS    Precision = "EB/s"
+	ZBSlashS    Precision = "ZB/s"
+	YBSlashS    Precision = "YB/s"
+	Nanosecond  Precision = "ns"
+	Microsecond Precision = "μs"
+	Millisecond Precision = "ms"
+	Second      Precision = "s"
+	ReqSlashS   Precision = "req/s"
+)
+
+type (
+	//Type .
+	Type string
+	//Precision .
+	Precision string
+	//DataStructure .
+	DataStructure struct {
+		Type      Type      `json:"type"`
+		Precision Precision `json:"precision"`
+		Enable    bool      `json:"enable"`
+	}
+)
