e56774145528365af89bc999d87c09c4961bb32a
sfwn <sfwnlin@gmail.com>
2022-05-24 17:59:34 +0800

    use buildx to build multi-arch gohub images (#279)

--- tools/dockerfile/build.sh
+++ tools/dockerfile/build.sh
@@ -8,7 +8,7 @@ set -o errexit -o pipefail
 cd $(git rev-parse --show-toplevel)
 
 # setup base image
-DOCKER_IMAGE=gohub:1.0.5
+DOCKER_IMAGE=gohub:latest
 
 if [ -n "${DOCKER_REGISTRY}" ]; then
     DOCKER_IMAGE=${DOCKER_REGISTRY}/${DOCKER_IMAGE}
@@ -19,7 +19,6 @@ usage() {
     echo "base_image.sh [ACTION]"
     echo "ACTION: "
     echo "    build       build docker image. this is default action."
-    echo "    push        push docker image, and build image if image not exist."
     echo "    build-push  build and push docker image."
     echo "    image       show image name."
     echo "Environment Variables: "
@@ -32,62 +31,40 @@ if [ -z "$1" ]; then
     usage
 fi
 
-# build docker image
-build_image() {
-    platforms="linux/amd64 linux/arm64"
-    for platform in ${platforms}; do
-        echo "building for $platform"
-        internal_build_image $platform
-    done
-}
+build_image_by_buildx() {
+    push=$1
+    args="docker buildx build"
+    args+=" -t ${DOCKER_IMAGE}"
+    args+=" --label build-time='$(date '+%Y-%m-%d %T%z')' --label debian=11 --label golang=1.17"
+    args+=" --platform linux/amd64,linux/arm64"
+    args+=" --progress plain"
+    args+=" -f ./tools/dockerfile/Dockerfile ."
+    if [[ "$push" == "true" ]]; then
+        args+=" --push"
+    fi
 
-# due to the issue that Aliyun Docker Registry doesn't support multi-arch under one docker tag,
-# we add targetplatform after original tag.
-internal_build_image() {
-    targetplatform=$1                              # linux/arm64
-    dash_targetplatform=${targetplatform//\//-}    # linux-arm64
-    image="${DOCKER_IMAGE}-${dash_targetplatform}" # {registry:}gohub:1.0.5-linux-arm64
-    docker build \
-        -t "$image" \
-        --label "build-time=$(date '+%Y-%m-%d %T%z')" \
-        --label "debian=11" \
-        --label "golang=1.17" \
-        --platform="$targetplatform" \
-        --progress=plain \
-        -f ./tools/dockerfile/Dockerfile .
+    echo "$args"
+    eval "$args"
 }
 
 # push docker image
-push_image() {
+docker_login() {
     if [ -z "${DOCKER_REGISTRY}" ]; then
         echo "fail to push docker image, DOCKER_REGISTRY is empty !"
         exit 1
     fi
-    IMAGE_ID="$(docker images ${DOCKER_IMAGE} -q)"
-    if [ -z "${IMAGE_ID}" ]; then
-        build_image
-    fi
     if [ -n "${DOCKER_REGISTRY_USERNAME}" ]; then
         docker login -u "${DOCKER_REGISTRY_USERNAME}" -p "${DOCKER_REGISTRY_PASSWORD}" ${DOCKER_IMAGE}
     fi
-    docker push "${DOCKER_IMAGE}"
-}
-
-# build and push
-build_push_image() {
-    build_image
-    push_image
 }
 
 case "$1" in
 "build")
-    build_image
-    ;;
-"push")
-    push_image
+    build_image_by_buildx false
     ;;
 "build-push")
-    build_push_image
+    docker_login
+    build_image_by_buildx true
     ;;
 "image")
     echo ${DOCKER_IMAGE}
