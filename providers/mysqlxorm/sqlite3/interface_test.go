7847b1f28676113c609be9f05c240d7b7e4d0b68
Malyue <86157286+Malyue@users.noreply.github.com>
2024-01-09 14:28:46 +0800

    add sqlite3-xorm provider (#310)
    * add sqlite3-xorm provider
    
    * add unit-test
    
    * rename sqlite3
    
    * add session rollback test

---
+++ providers/mysqlxorm/sqlite3/interface_test.go
@@ -0,0 +1,161 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sqlite3
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/xormplus/core"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/erda-project/erda-infra/providers/mysqlxorm"
+)
+
+const dbSourceName = "test1.sqlite3"
+
+type Server struct {
+	mysql mysqlxorm.Interface
+}
+
+type User struct {
+	ID   uint64 `json:"id" xorm:"pk autoincr"`
+	Name string `json:"name"`
+}
+
+func (u *User) TableName() string {
+	return "user"
+}
+
+func (s *Server) GetUserByID(id uint64, ops ...mysqlxorm.SessionOption) (*User, error) {
+	session := s.mysql.NewSession(ops...)
+	defer session.Close()
+
+	var user User
+	_, err := session.ID(id).Get(&user)
+
+	return &user, err
+}
+
+func (s *Server) CreateUser(user *User, ops ...mysqlxorm.SessionOption) (err error) {
+	session := s.mysql.NewSession(ops...)
+	defer session.Close()
+
+	_, err = session.Insert(user)
+	return err
+}
+
+func (s *Server) TestTx(err error, ops ...mysqlxorm.SessionOption) error {
+	session := s.mysql.NewSession(ops...)
+	defer session.Close()
+	return err
+}
+
+func TestNewSqlite3(t *testing.T) {
+	dbname := filepath.Join(os.TempDir(), dbSourceName)
+	defer func() {
+		os.Remove(dbname)
+	}()
+	engine, err := NewSqlite3(dbname)
+	if err != nil {
+		t.Fatalf("new sqlite3 err : %s", err)
+	}
+
+	server := Server{
+		mysql: engine,
+	}
+
+	server.mysql.DB().SetMapper(core.GonicMapper{})
+	server.mysql.DB().Sync2(&User{})
+
+	testCase := []struct {
+		name       string
+		insertUser []User
+		txErr      error
+		want       []User
+	}{
+		{
+			name:  "test tx",
+			txErr: errors.New("tx error"),
+			insertUser: []User{
+				{ID: 4, Name: "Alice"},
+				{ID: 5, Name: "Bob"},
+				{ID: 6, Name: "Cat"},
+			},
+			want: []User{},
+		},
+		{
+			name: "sqlite3 use for xorm",
+			insertUser: []User{
+				{ID: 1, Name: "Alice"},
+				{ID: 2, Name: "Bob"},
+				{ID: 3, Name: "Cat"},
+			},
+			txErr: nil,
+			want: []User{
+				{ID: 1, Name: "Alice"},
+				{ID: 2, Name: "Bob"},
+				{ID: 3, Name: "Cat"},
+			},
+		},
+	}
+
+	for _, test := range testCase {
+		t.Run(test.name, func(t *testing.T) {
+			tx := server.mysql.NewSession()
+			defer tx.Close()
+			if err = tx.Begin(); err != nil {
+				t.Fatalf("tx begin err : %s", err)
+			}
+
+			ops := mysqlxorm.WithSession(tx)
+			// insert sql
+			for _, user := range test.insertUser {
+				err = server.CreateUser(&user, ops)
+				if err != nil {
+					tx.Rollback()
+					t.Fatalf("create user err : %s", err)
+				}
+			}
+
+			err = server.TestTx(test.txErr, ops)
+			if err != nil {
+				tx.Rollback()
+			} else {
+				tx.Commit()
+			}
+
+			if len(test.want) <= 0 {
+				for _, user := range test.insertUser {
+					u, err := server.GetUserByID(user.ID)
+					if err != nil {
+						t.Fatalf("get user err : %s", err)
+					}
+					assert.Equal(t, &User{}, u)
+				}
+				return
+			}
+
+			for _, user := range test.want {
+				u, err := server.GetUserByID(user.ID)
+				if err != nil {
+					t.Fatalf("get user err : %s", err)
+				}
+				assert.Equal(t, user.Name, u.Name)
+			}
+		})
+	}
+}
---
+++ providers/mysqlxorm/sqlite3/provider.go
@@ -0,0 +1,64 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sqlite3
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/xormplus/xorm"
+
+	"github.com/erda-project/erda-infra/base/logs"
+	"github.com/erda-project/erda-infra/base/servicehub"
+	"github.com/erda-project/erda-infra/providers/mysqlxorm"
+)
+
+type config struct {
+	DbSourceName string `file:"dbSourceName" env:"DB_SOURCE_NAME" default:"test.sqlite3"`
+}
+
+type provider struct {
+	Cfg *config
+	Log logs.Logger
+	*Sqlite3
+}
+
+var _ servicehub.ProviderInitializer = (*provider)(nil)
+
+// Init .
+func (p *provider) Init(ctx servicehub.Context) error {
+	engine, err := xorm.NewSqlite3(p.Cfg.DbSourceName)
+	if err != nil {
+		return fmt.Errorf("failed to connect to sqlite3 engine,err : %s", err)
+	}
+
+	p.Sqlite3 = &Sqlite3{db: engine}
+
+	return nil
+}
+
+func init() {
+	servicehub.Register("sqlite3-xorm", &servicehub.Spec{
+		Services: []string{"sqlite3-xorm"},
+		Types: []reflect.Type{
+			reflect.TypeOf((*mysqlxorm.Interface)(nil)).Elem(),
+		},
+		Description: "sqlite3-xorm",
+		ConfigFunc:  func() interface{} { return &config{} },
+		Creator: func() servicehub.Provider {
+			return &provider{}
+		},
+	})
+}
